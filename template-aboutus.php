f07c82658819d9acda6f02c2b58ad54e94ef3a2a
Yuriy Zhmurko <777zhm@gmail.com>
2019-06-15 20:00:26 +0300

    about us page 2

--- template-aboutus.php
+++ template-aboutus.php
@@ -22,11 +22,11 @@
 			<div class="row">
 				<div class="col-md-6">
 					<div class="section_heading">
-						<h2>Title Trading</h2>
+						<h2>Oil trade</h2>
 					</div>
 					<div class="section_subtitle">
-						<p>With a proven personal track record behind us, we will engage in trading oil products and softs across regions as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-						<p>With a proven personal track record behind us, we will engage in trading oil products and softs across regions as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
+						<p>We are close to our customer.  In a trading environment where marginality has consistently eroded and where large trading houses are forced into large positions taking on paper, enormous physical volume term contracts, we at Centurion have decided to take another angle.</p>
+						<p>Proximity with our customers, be it when offering risk solutions, breaking bulk for effective shipment sizes, diversified supplies, enables us to understand their core demands.  We tailor-make solutions fit for every different customer.  Participating in niche markets, which are mostly left on the side by large trading houses, we even find cooperation possibilities with these trading houses.</p>
 					</div>
 				</div>
 				<div class="col-md-6">
@@ -39,26 +39,12 @@
 				</div>
 				<div class="col-md-6">
 					<div class="section_heading">
-						<h2>Title Trading</h2>
+						<h2>Softs trade</h2>
 					</div>
 					<div class="section_subtitle">
-						<p>With a proven personal track record behind us, we will engage in trading oil products and softs across regions as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-						<p>With a proven personal track record behind us, we will engage in trading oil products and softs across regions as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
+						<p>With a dedicated team of professionals in both Geneva and Kiev, Centurion is developing its softs desk.  Innovative financial instruments, down on the ground presence and thorough knowledge of local and international market players, gives Centurion a great base to trade softs.</p>
+						<p>By leveraging these relationships, we have decided to focus on two areas of trading: sourcing and CPT accumulation.  The first gives us an extraordinary insight as to what the market will be, and the CPT provides with real-time valuable market data to drive our overall decisions.</p>
 					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-6">
-					<div class="section_heading">
-						<h2>Title Trading</h2>
-					</div>
-					<div class="section_subtitle">
-						<p>With a proven personal track record behind us, we will engage in trading oil products and softs across regions as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-						<p>With a proven personal track record behind us, we will engage in trading oil products and softs across regions as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<img class="img img-fluid" src="<?php assets_url('img/placeholder_square.png') ?>" alt="placeholder_square">
 				</div>
 			</div>
 		</div>
@@ -69,24 +55,24 @@
 	<div class="founders_section">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-12">
+				<div class="col-md-12 text-center">
 					<div class="section_heading text-center">
 						<h2>Founders</h2>
 					</div>
+					<div class="founders_item_image_box">
+						<img class="img img-fluid" src="<?php assets_url('img/founders.jpg') ?>" alt="founders">
+					</div>
 				</div>
 				<div class="col-md-6 founder_item text-center">
-					<div class="founder_item_image_box">
-						<img class="img img-fluid founder_item_image" src="<?php assets_url('img/founder_koretskyi.jpg') ?>" alt="Sergii Koretskyi">
-					</div>
-					<h3>Sergii Koretskyi</h3>
-					<p>Sergii Koretskyi has an extensive experience in top management roles. Leading WOG as their CEO until 2019, he successfully managed a company of almost 8 000 people, one of Ukraine’s largest private firms with a turnover of 2 b $. His corporate approach, fine built financial expertise has allowed WOG to be amongst the strongest brands and corporations in Ukraine. ~12 000 corporate clients. Two crises in Ukraine.</p>
-				</div>
-				<div class="col-md-6 founder_item text-center">
-					<div class="founder_item_image_box">
-						<img class="img img-fluid founder_item_image" src="<?php assets_url('img/founder_nekhoda.jpg') ?>" alt="Ostap Nekhoda">
-					</div>
 					<h3>Ostap Nekhoda</h3>
-					<p>Ostap Nekhoda comes with over 10 years of trading experience. Of Ukrainian origin, he grew up and lives in Switzerland, where he has built a successful trading career. In his last position as Director and partner, he has participated in the fulgurant growth of one of Geneva’s latest success stories, Element. Trading and trade finance are at the core of his knowledge.</p>
+					<p>Ostap Nekhoda comes with over 10 years of trading experience. Of Ukrainian origin, he grew up and lives in Switzerland, where he has built a successful trading career.</p>
+					<p>In his last position as Director and partner, he has participated in the fulgurant growth of one of Geneva’s latest success stories, Element.</p>
+					<p>Trading and trade finance are at the core of his knowledge.</p>
+				</div>
+				<div class="col-md-6 founder_item text-center">
+					<h3>Sergii Koretskyi</h3>
+					<p>Sergii Koretskyi has an extensive experience in top management roles. Leading WOG as their CEO until 2019, he successfully managed a company of almost 8 000 people, one of Ukraine’s largest private firms with a turnover of 2 b $.</p>
+					<p>His corporate approach, fine built financial expertise has allowed WOG to be amongst the strongest brands and corporations in Ukraine with ~12 000 corporate clients and not only overcome but grow throughout two economic crises in Ukraine.</p>
 				</div>
 			</div>
 		</div>
