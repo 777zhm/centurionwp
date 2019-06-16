dea1277994b047a31620f59b46954da811f877f4
Yuriy Zhmurko <777zhm@gmail.com>
2019-06-16 16:50:08 +0300

    bubbles

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,40 @@ $ ('.hidden-link').click(function(){window.open($(this).data('link')); return fa
 }); //document.ready
 
 })( jQuery );
+
+var cnt1 = document.getElementById("count1");
+var cnt2 = document.getElementById("count2");
+var cnt3 = document.getElementById("count3");
+var water1 = document.getElementById("water1");
+var water2 = document.getElementById("water2");
+var water3 = document.getElementById("water3");
+var percent1 = cnt1.innerText;
+var percent2 = cnt2.innerText;
+var percent3 = cnt3.innerText;
+var interval1;
+var interval2;
+var interval3;
+interval1 = setInterval(function() {
+    percent1++;
+    cnt1.innerHTML = percent1 + 6;
+    water1.style.transform='translate(0'+','+(100-percent1)+'%)';
+    if (percent1 === 74){
+        clearInterval(interval1);
+    }
+}, 60);
+interval2 = setInterval(function() {
+    percent2++;
+    cnt2.innerHTML = percent2 + 6;
+    water2.style.transform='translate(0'+','+(100-percent2)+'%)';
+    if (percent2 === 56){
+        clearInterval(interval2);
+    }
+}, 60);
+interval3 = setInterval(function() {
+    percent3++;
+    cnt3.innerHTML = percent3 + 6;
+    water3.style.transform='translate(0'+','+(100-percent3)+'%)';
+    if (percent3 === 50){
+        clearInterval(interval3);
+    }
+}, 60);
--- template-bubles.php
+++ template-bubles.php
@@ -1,34 +1,7 @@
-<?php /* Template Name: Main page */ ?>
+<?php /* Template Name: Bubles page */ ?>
 <?php get_header(); ?>
 
 <section>
-	<div class="heroscreen_section mainpage_heroscreen">
-		<div class="container-fluid">
-			<div class="row justify-content-center">
-				<div class="col-md-12 page_heading text-center">
-					<h1>2018 Sustainability Report</h1>
-				</div>
-				<div class="col-md-6 section_subtitle text-center">
-					<p>Centurion is a Swiss-based trading company. With a proven personal track record behind us, <strong>we will engage in trading oil products and softs across regions</strong> as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<section>
-	<div class="container">
-		<div class="row justify-content-center text-center">
-			<div class="section_heading">
-				<h2>Centurion is a Swiss-based trading company.</h2>
-			</div>
-			<div class="col-md-6 section_subtitle">
-				<p>Centurion is a Swiss-based trading company. With a proven personal track record behind us, <strong>we will engage in trading oil products and softs across regions</strong> as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-			</div>
-		</div>
-	</div>
-</section>
-
-<section class="bubbles_section">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center  mb-5">
@@ -122,24 +95,5 @@
         </div>
     </div>
 </section>
-
-<section>
-	<div class="container">
-		<div class="row justify-content-center text-center">
-			<div class="col-md-6">
-				<img class="img img-fluid" src="<?php assets_url('img/office_illustration.png') ?>" alt="office_illustration">
-			</div>
-			<div class="col-md-6">
-				<div class="section_heading">
-					<h2>Why we are company</h2>
-				</div>
-				<div class="section_subtitle">
-					<p>Centurion is a Swiss-based trading company. With a proven personal track record behind us, <strong>we will engage in trading oil products and softs across regions</strong> as well as with customers that we have acquired in the 30+ years of combined trading experience.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
 
 <?php get_footer(); ?>
